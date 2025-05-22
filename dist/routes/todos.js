4ca9135075300f62b8c8c98a7917e14dfb84bdf2
keiukunnarpuu <annaliisa.tamme@voco.ee>
2025-05-22 21:29:51 +0300

    Get all Todos

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -4,8 +4,9 @@ const express_1 = require("express");
 const todos_1 = require("../controllers/todos");
 const router = (0, express_1.Router)();
 router.post('/', todos_1.createTodo);
-/* router.get('/')
-router.patch('/:id')
-router.delete('/:id') */ // - have to be commented when not in use
+router.get('/', todos_1.getTodos); // - have to be commented when not in use
+// router.patch('/:id')
+// router.delete('/:id') 
+// - have to be commented when not in use
 exports.default = router;
 // This code defines a router for handling HTTP requests related to "todos".
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import { createTodo } from '../controllers/todos';
+import { createTodo, getTodos } from '../controllers/todos';
 
 const router = Router();
 
 router.post('/', createTodo);
-/* router.get('/')
-router.patch('/:id')
-router.delete('/:id') */ // - have to be commented when not in use
+router.get('/', getTodos); // - have to be commented when not in use
+// router.patch('/:id')
+// router.delete('/:id') 
+// - have to be commented when not in use
 
 export default router;
 // This code defines a router for handling HTTP requests related to "todos".
