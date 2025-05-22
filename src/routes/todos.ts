f9bee7d8019256c23eef976644a2af39218c8876
keiukunnarpuu <annaliisa.tamme@voco.ee>
2025-05-22 19:46:01 +0300

    Create project basic files

---
+++ src/routes/todos.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.post('/')
+router.get('/')
+router.patch('/:id')
+router.delete('/:id')
+
+export default router;
+// This code defines a router for handling HTTP requests related to "todos".
